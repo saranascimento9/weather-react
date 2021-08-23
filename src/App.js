7e4ec776100094d25ae8f9fef3d3639db6c5ea9a
saranascimento9 <53081056+saranascimento9@users.noreply.github.com>
2021-08-23 20:36:35 +0100

    From codesandbox to VSCode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
+  let infoWeather = {
+    city: "Sintra",
+    date: "Sunday, 12:00",
+    temperature: "20",
+    description: "Few Clouds",
+    humidity: "80",
+    wind: "11",
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div class="container">
+        <div class="weather-app-wrapper">
+          <div class="weather-app">
+            <form id="search-form" class="mb-3">
+              <div class="row">
+                <input
+                  type="search"
+                  placeholder="type a city"
+                  class="form-control"
+                  id="city-input"
+                  autocomplete="off"
+                />
+              </div>
+              <div class="row">
+                <input type="submit" value="go" class="buttonsearch" />
+              </div>
+            </form>
+
+            <div class="overview">
+              <ul>
+                <li>Last updated:{infoWeather.date}</li>
+                <li id="description">{infoWeather.description}</li>
+              </ul>
+            </div>
+
+            <div class="row">
+              <div class="clearfix weather-temperature">
+                <h1 id="city">{infoWeather.city}</h1>
+                <div class="float-left">
+                  <strong id="temperature">{infoWeather.temperature}</strong>
+                  <span class="units"> ºC</span>
+                </div>
+                <img
+                  src="src/cloudy"
+                  alt="cloudy"
+                  id="icon"
+                  class="float-left"
+                />
+              </div>
+            </div>
+
+            <div class="row mt-3">
+              <ul class="weather-details">
+                <li>
+                  Humidity: <span id="humidity">{infoWeather.humidity}</span>%
+                </li>
+                <li>
+                  Wind: <span id="wind">{infoWeather.wind}</span>km/h
+                </li>
+              </ul>
+            </div>
+            <div class="weather-forecast" id="forecast"></div>
+          </div>
+
+          <small class="open-source-link">
+            <a
+              href="https://github.com/saranascimento9/weather-app-vanilla-week-7"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open Source code{" "}
+            </a>
+            by Sara Nascimento
+          </small>
+        </div>
+      </div>
+
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+    </body>
   );
 }
-
-export default App;
